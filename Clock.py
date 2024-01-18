cb9d6a19e07c62ffe53f17d28395956a80ec96d2
VishnuRajK-11 <vishnurajk2006@gmail.com>
2024-01-18 18:49:48 +0530

    Modified Clock

--- Clock.py
+++ Clock.py
@@ -1,15 +1,16 @@
 import time
 import tkinter as tk
+import random
 
 def update_time():
     current_time = time.strftime("%I:%M:%S %p")
-    label.config(text=current_time)
+    label.config(text=current_time ,foreground=f"#{random.randint(0, 0xFFFFFF):06x}")
     root.after(1000, update_time)
 
 root = tk.Tk()
 root.title("Clock")
 
-label = tk.Label(root, font=("ds-digital",80), background='black', foreground='cyan')
+label = tk.Label(root, font=("ds-digital",80), background='black')
 label.pack(anchor='center')
 
 update_time()
