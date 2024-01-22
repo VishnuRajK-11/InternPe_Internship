a7ff76e5e9ad9a33d49fbcd85b281b81a42a1e56
VishnuRajK-11 <vishnurajk2006@gmail.com>
2024-01-22 19:49:40 +0530

    Created tic tac toe

--- Clock.py
+++ Clock.py
@@ -4,7 +4,7 @@ import random
 
 def update_time():
     current_time = time.strftime("%I:%M:%S %p")
-    label.config(text=current_time ,foreground=f"#{random.randint(0, 0xFFFFFF):06x}")
+    label.config(text=current_time,foreground=f"#{random.randint(0, 0xFFFFFF):06x}")
     root.after(1000, update_time)
 
 root = tk.Tk()
